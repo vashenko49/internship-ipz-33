bff2ef5e9c71492739937d51975e7ba05a8774a4
Vashchenko Sergey Aleksandrovich <vashenko49@gmail.com>
2020-02-18 00:27:10 +0200

    mini fixed

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -6,12 +6,14 @@ import HomePage from '../HomePage/HomePage';
 import PersonalProfile from '../PersonalData/PersonalData';
 import PrivateRoute from './PrivateRoute';
 import Chat from '../Chat/Chat';
+import PasswordRecovery from '../Authorization/PasswordRecovery/PasswordRecovery';
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/authorization*" component={AuthorizationPage} />
+        <Route exact path="/passwordrecovery/:token" component={PasswordRecovery} />
         <Route exact path="/personaldata" component={PersonalProfile} />
         <PrivateRoute exact path="/chat" component={Chat} />
         <Route exact path="/" component={HomePage} />
